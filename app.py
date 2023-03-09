430c9f781ec280a599dd47252897b854836c460f
anas nashat <72121781+anasnashat@users.noreply.github.com>
2023-03-09 07:50:40 +0200

    create the endpoints

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from filters import bp as filters_pb
+from action import bp as action_pb
+from android import bp as android_pb
+
 
 app = Flask(__name__)
 
 # genarte secret key
 app.secret_key = "54fasdf5484fsdf5asdf874fasd"
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+app.register_blueprint(filters_pb)
+app.register_blueprint(action_pb)
+app.register_blueprint(android_pb)
 
 if __name__ == '__main__':
     app.run()
