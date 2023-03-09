22283972f2a53f3bc4870c2aa152c0fb9b401ea5
anas nashat <72121781+anasnashat@users.noreply.github.com>
2023-03-09 08:35:00 +0200

    Upload Files To Local Server

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from filters import bp as filters_pb
 from action import bp as action_pb
 from android import bp as android_pb
-
+from helpers import allowed_extension, get_secure_filename_filepath
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSION = ['png', 'jpg', 'jpeg']
 
-# genarte secret key
 app.secret_key = "54fasdf5484fsdf5asdf874fasd"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSION'] = ALLOWED_EXTENSION
 
 app.register_blueprint(filters_pb)
 app.register_blueprint(action_pb)
 app.register_blueprint(android_pb)
+
+
+@app.route('/images', methods=['POST'])
+def upload_images():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'no file was selected'}), 400
+
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({"Error": "No File Was Selected"}), 400
+
+        if not allowed_extension(file.filename):
+            return jsonify({'error': 'the extension not supported'}), 400
+
+        filename, filepath = get_secure_filename_filepath(file.filename)
+
+        file.save(filepath)
+
+        return jsonify({
+            'success': 'file uploaded successfully',
+            'filename': filename
+        }), 201
+
 
 if __name__ == '__main__':
     app.run()
